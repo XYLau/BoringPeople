44a8eb9694cb9222a4a3c249f28fcd021b4a8f6b
XYLau <a0114573@nus.edu.sg>
2014-11-04 16:38:33 +0800

    BookingDetails

---
+++ BookingDetails.php
@@ -0,0 +1,20 @@
+<html>
+<head><title>Booking Details</title></head>
+<body>
+<table>
+	<tr><td colspan="2" style="background-color:#FFA500;">
+<h1> Demo Book Catalog</h1>
+<?php
+<tr>
+<td> echo "Name: " .$_POST['name'] " <br>
+ Contact number: " .$_POST['contact'] " <br> 
+ Email: " .$_POST['email'] " <br>
+ Country of departure: " .$_POST[''] " <br>
+ Flight Number: " .$_POST[''] " <br>
+ Seat Number: " .$_POST[''] " <br>"; 
+ </td> 
+ </tr>
+ ?>
+</table>
+</body>
+</html>
